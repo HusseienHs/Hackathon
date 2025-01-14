f03e9b6194264ca4163f411da0b4a46350f979f0
Hussien2605 <168656898+Hussien2605@users.noreply.github.com>
2025-01-14 16:13:43 +0200

    client and server and shared files

--- client.py
+++ client.py
@@ -3,30 +3,7 @@ import struct
 import threading
 import time
 
-MAGIC_COOKIE = 0xabcddcba
-OFFER_TYPE = 0x2
-REQUEST_TYPE = 0x3
-PAYLOAD_TYPE = 0x4
-
-# ANSI Color Codes
-RED = "\033[91m"
-GREEN = "\033[92m"
-YELLOW = "\033[93m"
-CYAN = "\033[96m"
-RESET = "\033[0m"
-
-
-def create_offer_packet(udp_port, tcp_port):
-    return struct.pack('!IBHH', MAGIC_COOKIE, OFFER_TYPE, udp_port, tcp_port)
-
-
-def create_request_packet(file_size):
-    return struct.pack('!IBQ', MAGIC_COOKIE, REQUEST_TYPE, file_size)
-
-
-def create_payload_packet(total_segments, current_segment, payload):
-    header = struct.pack('!IBQQ', MAGIC_COOKIE, PAYLOAD_TYPE, total_segments, current_segment)
-    return header + payload
+from shared import create_offer_packet, create_request_packet, create_payload_packet, MAGIC_COOKIE, OFFER_TYPE, REQUEST_TYPE, PAYLOAD_TYPE, RED, GREEN, YELLOW, RESET, CYAN
 
 
 class Client:
